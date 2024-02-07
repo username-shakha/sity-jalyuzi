573f0981af293d9cd3e238c93bdbd629456776d3
username-shakha <username.shakha@gmail.com>
2024-02-07 16:24:18 +0500

    CREATED: Header Component

---
+++ src/pages/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import "./Header.scss";
+
+const Header: FC = () => {
+  return (
+    <div className="header">
+      <h1 className="header__title">Header</h1>
+    </div>
+  );
+};
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 const Layout: FC = () => {
   return (
     <div className="wrapper">
+      <Header />
       <div className="push"></div>
       <main className="main">
         <Outlet />
