375526ecc34b6699144e14e9c4a6b1c49f0533ff
username-shakha <username.shakha@gmail.com>
2024-02-08 13:25:20 +0500

    CREATE: ResponsiveCarousel Component

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import banner1 from "./images/banner1.svg";
+import banner2 from "./images/banner2.svg";
+import banner3 from "./images/banner3.svg";
+
+export { banner1, banner2, banner3 };
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,7 +1,12 @@
+import ResponsiveCarousel from "../ResponsiveCarousel";
+import { banner1, banner2, banner3 } from "@/assets";
+
 const Hero = () => {
   return (
-    <section className="banner">
-      <div className="banner__container">Hero</div>
+    <section className="hero">
+      <div className="hero__container">
+        <ResponsiveCarousel images={[banner1, banner2, banner3]} />
+      </div>
     </section>
   );
 };
