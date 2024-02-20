4b18c2add66233ee7b1d366bca4cf451ea21c969
username-shakha <username.shakha@gmail.com>
2024-02-20 17:38:45 +0500

    created contact page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./pages";
 import Home from "./pages/Home";
 import Delivery from "./pages/Delivery";
+import Contact from "./pages/Contact";
 
 const App: FC = (): JSX.Element => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />} index />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/delivery" element={<Delivery />} />
       </Route>
     </Routes>
