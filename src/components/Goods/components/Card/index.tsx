761b006ca9ef0623bea287ad6b70eadb745d65b5
username-shakha <username.shakha@gmail.com>
2024-02-21 17:38:37 +0500

    bootstrap.min.css fix meta link

--- src/components/Goods/components/Card/index.tsx
+++ src/components/Goods/components/Card/index.tsx
@@ -10,14 +10,14 @@ const Card = ({ product }: ICardProps) => {
   const navigate = useNavigate();
 
   return (
-    <div className="we-produce__col">
+    <div className="like__col">
       <div
-        className="we-produce__item"
+        className="like__item"
         onClick={() => {
           if (product) navigate(`/details/${product.id}`);
         }}
       >
-        <div className="we-produce__item-photo">
+        <div className="like__item-photo">
           <img
             data-src="https://city-jaluzi.ru/storage/2023/12/14/d89e2b403e97110109cb21a9c03826a79252a49a.webp"
             alt="Вертикальные тканевые жалюзи МАЛЬТА 5992 бирюзовый 89 мм"
@@ -27,22 +27,17 @@ const Card = ({ product }: ICardProps) => {
 
           <div className="buy__item-discount">{product.discount}</div>
 
-          <div className="we-produce__item-preview">
-            <div className="we-produce__item-preview-text">
-              {" "}
-              Быстрый просмотр
-            </div>
+          <div className="like__item-preview">
+            <div className="like__item-preview-text"> Быстрый просмотр</div>
           </div>
         </div>
-        <div className="we-produce__item-content">
-          <div className="we-produce__item-cost">
-            <div className="we-produce__item-new">{product.sale_price}</div>
+        <div className="like__item-content">
+          <div className="like__item-cost">
+            <div className="like__item-new">{product.sale_price}</div>
 
             <div className="buy__item-old">{product.price}</div>
           </div>
-          <div className="we-produce__item-discription">
-            {product.description}
-          </div>
+          <div className="like__item-discription">{product.description}</div>
         </div>
       </div>
     </div>
