80709584ab4649f7d3b774b64a29776df99f1f30
username-shakha <username.shakha@gmail.com>
2024-02-21 20:40:42 +0500

    change

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+//config
 export default defineConfig({
   plugins: [react()],
   resolve: {
