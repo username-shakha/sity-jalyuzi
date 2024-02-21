d08b260b47cd0b58f004d0d914127e6d7827c2e3
username-shakha <username.shakha@gmail.com>
2024-02-21 14:05:22 +0500

    created requisites page

--- src/pages/Contact/Requisites.tsx
+++ src/pages/Contact/Requisites.tsx
@@ -1,5 +1,67 @@
 import { FC } from "react";
 
 export const Requisites: FC = () => {
-  return <div>Requisites</div>;
+  return (
+    <section className="delivery">
+      <div className="information__container">
+        <h2 className="information__title">Реквизиты</h2>
+        <section className="">
+          <h1 className="section__title"></h1>
+          <div className="section__inner">
+            <p>Наименование организации: ООО «Авалон»</p>
+
+            <p>Юридический адрес: 125195, Москва, ул. Смольная, д.51, корп.1</p>
+
+            <p>
+              Фактический адрес: 109369, Москва, Новочеркасский бульвар, д.41,
+              корп.3
+            </p>
+
+            <p>&nbsp;</p>
+
+            <p>ИНН/КПП: 7743877771/774301001</p>
+
+            <p>Банк: ПАО «БАНК УРАЛСИБ»</p>
+
+            <p>Р/с: 40702810300280000180</p>
+
+            <p>К/с: 30101810100000000787</p>
+
+            <p>БИК: 044525787</p>
+
+            <p>ОГРН: 1137746090371</p>
+
+            <p>ОКАТО: 45277583000</p>
+
+            <p>ОКПО: 17154718</p>
+
+            <p>ОКВЭД: 25.23, 17.4,17.5, 18.2, 52.4, 72.6</p>
+
+            <p>ОКТМО: 45342000</p>
+
+            <p>ОКОГУ: 4210014</p>
+
+            <p>ОКФС: 16</p>
+
+            <p>ОКОПФ: 12165</p>
+
+            <p>&nbsp;</p>
+
+            <p>Генеральный директор Русанов Виктор Леонидович</p>
+
+            <p>Телефон: 8(495)5427698</p>
+
+            <p>
+              E-mail:{" "}
+              <a href="mailto:info@city-jaluzi.ru">info@city-jaluzi.ru</a>
+            </p>
+
+            <p>
+              Web: <a href="">http://city-jaluzi.ru</a>
+            </p>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
 };
